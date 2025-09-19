1f9930c6e7740e23d5ca1fceb7e6ba630171f7b7
orlando <bowcotto297@kesbath.com>
2025-09-19 14:31:25 +0100

    first question

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Console_Blank_6
+﻿using System.ComponentModel.Design;
+
+namespace Console_Blank_6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number;
+            Console.Write("enter an integer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter another integer: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2)
+            {
+                number = num1 / num2;
+            }
+            else
+            {
+                number = num2 / num1;
+            }
+            int count = 0;
+            while (count != number)
+            {
+                count++;
+                if (count %10 == 0)
+                {
+                    Console.Write("X");
+                }
+                else if (count % 5 == 0)
+                {
+                    Console.Write("V");
+                }
+                else
+                {
+                    Console.Write("/");
+                }
+            }
         }
     }
 }
